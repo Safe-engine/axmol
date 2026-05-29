896967ed9cc5bf8759b1870b34a44b3f35547347
antn1 <antn@ftech.com.vn>
2026-05-29 13:41:06 +0700

    fix js script

--- tests/js-tests/Content/src/index.js
+++ tests/js-tests/Content/src/index.js
@@ -6,29 +6,6 @@ global.__axFile = 42;
 if (global.__axRunImageSpriteTest) {
     global.__axImageSpriteStage = 1;
 
-class ActionScene extends Scene {
-    onEnter() {
-        global.__axImageSpriteStage = 2;
-        console.log('ActionScene onEnter');
-        super.onEnter();
-
-        const size = view.getDesignResolutionSize();
-        const sprite = Sprite.create('res/Hello.png');
-        if (!sprite) {
-            global.__axImageSpriteSpriteNull = 1;
-            return;
-        }
-
-        global.__axImageSpriteStage = 3;
-        sprite.setPosition(size.width / 2, size.height / 2);
-        this.addChild(sprite);
-
-        const moveAction = moveBy(5, p(100, 600));
-        sprite.runAction(moveAction);
-        global.__axImageSpriteTest = 1;
-    }
-}
-
 global._isContextMenuEnable = true;
 game.run(
     {
@@ -48,14 +25,28 @@ game.run(
         view.setDesignResolutionSize(width, height, policy);
         view.resizeWithBrowserSize(true);
 
-        const scene = new ActionScene();
+        const scene = new Scene();
         if (!scene) {
             global.__axImageSpriteSceneNull = 1;
             return;
         }
+        global.__axImageSpriteStage = 2;
+        // console.log('Scene onEnter');
+        const size = view.getDesignResolutionSize();
+        const sprite = Sprite.create('res/Hello.png');
+        if (!sprite) {
+            global.__axImageSpriteSpriteNull = 1;
+            return;
+        }
 
-        // scene.onEnter();
+        global.__axImageSpriteStage = 3;
+        sprite.setPosition(size.width / 2, size.height / 2);
+        scene.addChild(sprite);
+
+        const moveAction = moveBy(5, p(100, 600));
+        sprite.runAction(moveAction);
+        global.__axImageSpriteTest = 1;
         director.runScene(scene);
     }
 );
-}
+};
